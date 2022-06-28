08ddc34edabefaa3fd8bdc653cd5df9d90fd323f
Brandon Kiefer <brandon.kiefer@ark.org>
2022-06-28 13:09:15 -0500

    ENV improvements, service updates

--- mongo.init.d/xhgui.js
+++
@@ -1,6 +0,0 @@
-db.results.createIndexes( { 'meta.SERVER.REQUEST_TIME' : -1 } );
-db.results.createIndexes( { 'profile.main().wt' : -1 } );
-db.results.createIndexes( { 'profile.main().mu' : -1 } );
-db.results.createIndexes( { 'profile.main().cpu' : -1 } );
-db.results.createIndexes( { 'meta.url' : 1 } );
-db.results.createIndexes( { 'meta.SERVER.SERVER_NAME' : 1 } );
---
+++ xhprof/mongo.init.d/xhgui.js
@@ -0,0 +1,6 @@
+db.results.createIndex( { 'meta.SERVER.REQUEST_TIME' : -1 } );
+db.results.createIndex( { 'profile.main().wt' : -1 } );
+db.results.createIndex( { 'profile.main().mu' : -1 } );
+db.results.createIndex( { 'profile.main().cpu' : -1 } );
+db.results.createIndex( { 'meta.url' : 1 } );
+db.results.createIndex( { 'meta.SERVER.SERVER_NAME' : 1 } );
